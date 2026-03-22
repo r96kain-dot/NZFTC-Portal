5a7f0e31484357d414eb14fc233489e4a45ada61
happyou2107 (Richard) <r96kain@gmail.com>
2026-03-22 20:31:59 +1300

    Updated Program.cs to launch from the Login page, was launching to Employee Dashboard during testing.

--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Employee}/{action=Dashboard}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
